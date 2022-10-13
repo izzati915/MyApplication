2babb5f7ea73727cbabad5f4da020f27b54f0f6d
Nur Izzati Binti Ishak <101409995+izzati915@users.noreply.github.com>
2022-10-13 10:53:08 +0800

    Add files via upload

---
+++ src/main/java/ftmk/bitp3453/helloClass/MainActivity.java
@@ -0,0 +1,14 @@
+package ftmk.bitp3453.helloClass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
